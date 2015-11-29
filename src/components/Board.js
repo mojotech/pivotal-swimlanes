a73a39d2cb6d3e90e49c64604f71263b144a3fe7
Dylan Kirby <dylan@mojotech.com>
2015-11-28 21:13:55 -0500

    simplify Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,12 +3,6 @@ import SwimLane from './SwimLane';
 import _ from 'lodash';
 
 const Board = ({ entries }) => {
-  const unstartedEntries = _.filter(entries, entry => entry.state === 'Unstarted');
-  const inProgressEntries = _.filter(entries, entry => entry.state === 'In Progress');
-  const readyForReviewEntries = _.filter(entries, entry => entry.state === 'Ready for Review');
-  const mergedEntries = _.filter(entries, entry => entry.state === 'Merged');
-  const deliveredEntries = _.filter(entries, entry => entry.state === 'Delivered');
-  const acceptedEntries = _.filter(entries, entry => entry.state === 'Accepted');
   const styles = {
     board: {
       overflow: 'scroll',
@@ -19,15 +13,17 @@ const Board = ({ entries }) => {
       width: '100%'
     }
   };
-
+  const categories = [
+    'Unstarted', 'In Progress', 'Ready for Review', 'Merged', 'Delivered', 'Accepted'
+  ];
   return (
     <div style={styles.board}>
-      <SwimLane title='Unstarted' entries={unstartedEntries} />
-      <SwimLane title='In Progress' entries={inProgressEntries} />
-      <SwimLane title='Ready for Review' entries={readyForReviewEntries} />
-      <SwimLane title='Merged' entries={mergedEntries} />
-      <SwimLane title='Delivered' entries={deliveredEntries} />
-      <SwimLane title='Accepted' entries={acceptedEntries} />
+      {_.map(categories, (category, i) => (
+        <SwimLane
+          key={i}
+          title={category}
+          entries={_.filter(entries, entry => entry.state === category)} />
+      ))}
     </div>
   );
 };
