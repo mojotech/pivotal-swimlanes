f50fe09f2e2b10ea2aef2a37cb5cc7cfa07a00b4
Dylan Kirby <dylan@mojotech.com>
2016-08-04 22:20:33 -0400

    remove all files

--- src/settings.js
+++
@@ -1,9 +0,0 @@
-const localStorageKey = 'pivotal-swimlanes-config';
-
-const getSettings = () => JSON.parse(localStorage.getItem(localStorageKey)) || {};
-
-const updateSettings = data => (
-  localStorage.setItem(localStorageKey, JSON.stringify({ ...getSettings(), ...data }))
-);
-
-export default { getSettings, updateSettings };
