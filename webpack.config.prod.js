869af7a968f637f930a63fa2d4436d611903cd3e
Cathy Zoller <cathy@mojotech.com>
2017-03-08 12:55:17 -0700

    update production webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,36 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
+var cssLoaders = [
+  {
+    loader: 'css-loader'
+  },
+  {
+    loader: 'sass-loader',
+    options: {
+      includePaths: join('/node_modules')
+    }
+  }
+];
+var imgLoaders = [
+  { loader: 'file-loader' },
+  { loader: 'url-loader',
+    options: {
+      limit: 10000,
+      name: '../images/[name].[ext]'
+    }
+  }
+];
+
+function join(dest) { return path.resolve(__dirname, dest); }
+
+function web(dest) { return join('web/static/' + dest); }
 
 module.exports = {
   devtool: 'source-map',
   entry: {
     application: [
+      'babel-polyfill',
       web('css/application.scss'),
       web('js/application.js')
     ]
@@ -31,21 +56,23 @@ module.exports = {
     new ExtractTextPlugin('css/application.css')
   ],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader']
+        exclude: [/node_modules/],
+        use: 'babel-loader'
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(
-          'style', 'css-loader!sass-loader?includePaths[]=' + __dirname +  '/node_modules'
-        )
+        use: ExtractTextPlugin.extract({
+          use: cssLoaders,
+          fallback: 'style-loader'
+        })
       },
-      { test: /\.(svg|png)$/i,
-        exclude: /node_modules/,
-        loaders: ['file-loader','url-loader?limit=10000&name=../images/[name].[ext]']
+      {
+        test: /\.(svg|png)$/i,
+        exclude: [/node_modules/],
+        use: imgLoaders
       }
     ]
   }
